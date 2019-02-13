b0a97faa9016f8f53ad6cbe6adf87c2a6977264c
mdche001 <mdche@bu.edu>
2019-02-14 06:00:01 +0800

    thread and requirements

--- sms_sender.py
+++ sms_sender.py
@@ -3,9 +3,14 @@
 # Copyright Mingdao Che mdche@bu.edu 2019#
 # Reference: https://www.twilio.com/docs/sms/quickstart/python
 
+
 from twilio.rest import Client
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import threading
+import time
+import random
+
 
 # data type from the common_types
 class Message(object):
@@ -21,11 +26,11 @@ class Message(object):
 
 
 # Your Account Sid and Auth Token from twilio.com/console
-# accountSID='authToken'
-# authToken='authToken'
-# myNumber='myNumber'
-# twilioNumber='twilioNumber'
-# message= "" #Message you want to send
+accountSID='authToken'
+authToken='authToken'
+myNumber='+1mynum'
+twilioNumber='+1twilionum'
+message= "Your blood pressure is 100/120"
 
 # for general information #
 class Contact(object):
@@ -45,13 +50,15 @@ class Contact(object):
 
 
 
-def textmyself(source_phone_num, destination_phone_num,message):
+def textmyself(source_phone_num,destination_phone_num,message):
     """fuction to  send messages to the phone by twilio number"""
     # Getting the Authorization of the twilio
-    twilioCli = Client(accountSID,authToken)
+    delay = random.randrange(1, 100)
+    time.sleep(delay)
 
     #send message by twilio api
     # twilioCli.api.account.messages.create(body=message,from_=twilioNumber,to=myNumber)//python2
+    twilioCli = Client(accountSID, authToken)
     twilioCli.messages.create(body=message,from_=twilioNumber,to=myNumber)#python3
 
 
@@ -62,34 +69,36 @@ def callmyself(source_phone_num, destination_phone_num,message):
     # call the phone number
     testcall = twilioCli.calls.create(
                             url='http://demo.twilio.com/docs/voice.xml', # the trial voice's url
-                            to='source_phone_num',   #your twilio num
-                            from_='destination_phone_num' #any phone num you want
+                            to='+18574728393',
+                            from_='+18572148494'
                         )
 
     print(testcall.sid)
 
 
-# @app.route("/")
-# def hello():
-#     return "Hello World!"
-
-# replying message by flask
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def sms_reply():
+def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
     # Start our response
     resp = MessagingResponse()
 
     # Add a message
-    resp.message("Thanks for you reply, we'll deal with it as soon as possible!")
+    resp.message("Thanks so much for your reply, we will contact you as soon as possible!")
 
     return str(resp)
 
 if __name__ == "__main__":
-    Msg = Message ('The patient is blooding', 0)
+    # Msg = Message ('The patient is blooding', 0)
     # textmyself(myNumber,twilioNumber,message)
+    # while(True):
     callmyself(myNumber,twilioNumber,message)
-    textmyself(myNumber,twilioNumber,Msg.get_msg_content())
+    # for i in range(5):
+    #     _thread.start_new_thread(textmyself, (myNumber,twilioNumber,message))
+
+    for i in range(5):
+        t = threading.Thread(target=textmyself(myNumber,twilioNumber,message))
+        t.start()
+    # textmyself(Msg.get_msg_content()) # From the common types
     app.run(debug=True)
